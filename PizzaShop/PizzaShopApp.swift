f7ed5818543f049e484ff78c216c6c6efc24ee5c
Mikhail Zhdanov <mikhail.zhdanov@elementpay.io>
2025-01-11 19:13:38 +0700

    Created Xcode project

---
+++ PizzaShop/PizzaShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaShopApp.swift
+//  PizzaShop
+//
+//  Created by Михаил Жданов on 11.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct PizzaShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
